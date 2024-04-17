3f145cd4c74cb6916186eb0a91ba30d01cb75d22
AthenaSui <wangmz29@163.com>
2024-04-17 09:30:56 +0800

    f

--- data/sql/custom/db_world/2023_05_20_00_creature_template.sql
+++ data/sql/custom/db_world/2023_05_20_00_creature_template.sql
@@ -5,13 +5,7 @@ SET @BOT_END   = 70595;
 DELETE FROM `creature_template` WHERE `entry` BETWEEN @BOT_START AND @BOT_END;
 
 INSERT INTO `creature_template`
-(`entry`,`difficulty_entry_1`,`difficulty_entry_2`,`difficulty_entry_3`,`KillCredit1`,`KillCredit2`,
-`modelid1`,`modelid2`,`modelid3`,`modelid4`,`name`,`subname`,`IconName`,`gossip_menu_id`,`minlevel`,`maxlevel`,`exp`,
-`faction`,`npcflag`,`speed_walk`,`speed_run`,`scale`,`rank`,`dmgschool`,`BaseAttackTime`,`RangeAttackTime`,
-`BaseVariance`,`RangeVariance`,`unit_class`,`unit_flags`,`unit_flags2`,`dynamicflags`,`family`,`type`,`type_flags`,`lootid`,
-`pickpocketloot`,`skinloot`,`PetSpellDataId`,`VehicleId`,`mingold`,`maxgold`,`AIName`,`MovementType`,
-`HoverHeight`,`HealthModifier`,`ManaModifier`,`ArmorModifier`,`DamageModifier`,`ExperienceModifier`,`RacialLeader`,`movementId`,
-`RegenHealth`,`mechanic_immune_mask`,`spell_school_immune_mask`,`flags_extra`,`ScriptName`,`VerifiedBuild`)
+(`entry`,`difficulty_entry_1`,`difficulty_entry_2`,`difficulty_entry_3`,`KillCredit1`,`KillCredit2`,`modelid1`,`modelid2`,`modelid3`,`modelid4`,`name`,`subname`,`IconName`,`gossip_menu_id`,`minlevel`,`maxlevel`,`exp`,`faction`,`npcflag`,`speed_walk`,`speed_run`,`speed_swim`,`speed_flight`,`detection_range`,`scale`,`rank`,`dmgschool`,`DamageModifier`,`BaseAttackTime`,`RangeAttackTime`,`BaseVariance`,`RangeVariance`,`unit_class`,`unit_flags`,`unit_flags2`,`dynamicflags`,`family`,`trainer_type`,`trainer_spell`,`trainer_class`,`trainer_race`,`type`,`type_flags`,`lootid`,`pickpocketloot`,`skinloot`,`PetSpellDataId`,`VehicleId`,`mingold`,`maxgold`,`AIName`,`MovementType`,`HoverHeight`,`HealthModifier`,`ManaModifier`,`ArmorModifier`,`ExperienceModifier`,`RacialLeader`,`movementId`,`RegenHealth`,`mechanic_immune_mask`,`spell_school_immune_mask`,`flags_extra`,`ScriptName`,`VerifiedBuild`)
 VALUES
 (70587, 0, 0, 0, 0, 0, 27395, 0, 0, 0, '图特纳拉克', '地穴领主', '', 0, 83, 83, 2, 35, 1, 1, 1, 1, 1, 20, 0.5, 2, 0, 1, 1900, 1900, 1, 1, 1, 0, 32, 0, 0, 0, 0, 0, 0, 6, 4096, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2383413360, 'crypt_lord_bot', -1),
 (70588, 0, 0, 0, 0, 0, 27395, 0, 0, 0, '阿努比洛斯', '地穴领主', '', 0, 83, 83, 2, 35, 1, 1, 1, 1, 1, 20, 0.5, 2, 0, 1, 1900, 1900, 1, 1, 1, 0, 32, 0, 0, 0, 0, 0, 0, 6, 4096, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2383413360, 'crypt_lord_bot', -1),
